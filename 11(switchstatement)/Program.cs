e996b0a17e727ff8221b565ea4ac0588d76b5167
iampr <iampr@PRABIN>
2023-01-04 22:58:39 +0545

    Add project files.

---
+++ 11(switchstatement)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a Number ");
+       int UserNumber = int.Parse(Console.ReadLine());
+        switch(UserNumber)
+        {
+            case 10:
+            case 20:
+            case 30:
+                Console.WriteLine("Your number is {0}", UserNumber);
+                break;
+             default: Console.WriteLine("Your number is not 10 , 20 and 30 "); break;
+
+        }
+      
+       
+    }
+}
